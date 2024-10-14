fa438d4832323322b15e5f2463570035e5ab979a
pentaery <pentaerythrotol@gmail.com>
2024-10-14 19:49:11 +0800

    first cmake

---
+++ HelloOpenMP/HelloOpenMP.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main(int argc, char *argv[]) {
+  #pragma omp parallel
+  {
+    int nthreads, thread_id;
+    nthreads = omp_get_num_threads();
+    thread_id = omp_get_thread_num();
+  #pragma omp single
+    {
+      printf("Goodbye slow serial world and Hellp OpenMP!\n");
+      printf("I have %d thread(s) and my thread id is %d\n", nthreads,
+             thread_id);
+    }
+  }
+}
